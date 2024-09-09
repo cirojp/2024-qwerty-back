24afbbf1d3245cc18ae54c6d27eaf420b57ba615
jp ciro <jciro@itba.edu.ar>
2024-09-09 18:56:49 -0300

    agregar tipo de transaccion personal por usuario

---
+++ repository/IngSoft2-Model/back/src/main/java/api/back/PersonalTipoGastoService.java
@@ -0,0 +1,31 @@
+package api.back;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PersonalTipoGastoService {
+    
+    @Autowired
+    private PersonalTipoGastoRepository personalTipoGastoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<PersonalTipoGasto> getPersonalTipoGastos(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return personalTipoGastoRepository.findByUser(user);
+    }
+
+    public PersonalTipoGasto addPersonalTipoGasto(String email, String nombre) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        PersonalTipoGasto tipoGasto = new PersonalTipoGasto();
+        tipoGasto.setNombre(nombre);
+        tipoGasto.setUser(user);
+        return personalTipoGastoRepository.save(tipoGasto);
+    }
+
+    // si dsps necesitamos, agregamos eliminar o editar
+}

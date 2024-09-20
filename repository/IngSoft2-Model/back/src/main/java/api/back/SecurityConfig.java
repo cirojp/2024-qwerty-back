b8709c02f391aeb3b7e75c2884ea1c568b6d5a1f
jp ciro <jciro@itba.edu.ar>
2024-09-20 12:31:04 -0300

    hosteo

--- repository/IngSoft2-Model/back/src/main/java/api/back/SecurityConfig.java
+++ repository/IngSoft2-Model/back/src/main/java/api/back/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+                    corsConfiguration.setAllowedOrigins(List.of("https://2024-qwerty-front-2.vercel.app/", "http://127.0.0.1:5173"));
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(List.of("*"));
                     corsConfiguration.setAllowCredentials(true);

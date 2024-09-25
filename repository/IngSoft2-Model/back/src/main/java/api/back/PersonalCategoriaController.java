4dbc9105ba958f19ae6201ee5a799c9b5565a868
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-25 16:01:49 -0300

    Fixed EditCategoria Endpoint

--- repository/IngSoft2-Model/back/src/main/java/api/back/PersonalCategoriaController.java
+++ repository/IngSoft2-Model/back/src/main/java/api/back/PersonalCategoriaController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,14 +55,14 @@ public class PersonalCategoriaController {
         }
     }
 
-    @PutMapping
-    public ResponseEntity<Void> editPersonalCategoria(@RequestBody CategoriaRequest categoria,
+    @PutMapping("/{nombre}")
+    public ResponseEntity<Void> editPersonalCategoria(@PathVariable String nombre,
             @RequestBody CategoriaRequest newCategoria, Authentication authentication) {
         try {
             String email = authentication.getName();
             List<PersonalCategoria> categorias = personalCategoriaService.getPersonalCategoria(email);
             for (PersonalCategoria item : categorias) {
-                if (item.getNombre().equals(categoria.getNombre())) {
+                if (item.getNombre().equals(nombre)) {
                     System.out.println("Found: " + item);
                     item.setNombre(newCategoria.getNombre());
                     item.setIconPath(newCategoria.getIconPath());

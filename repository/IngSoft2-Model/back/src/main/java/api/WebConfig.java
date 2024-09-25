e4759f8a5fd1db3e2754fc3ddb862f74f0cd639d
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-25 01:16:42 -0300

    Fixed localhost Origin

--- repository/IngSoft2-Model/back/src/main/java/api/WebConfig.java
+++ repository/IngSoft2-Model/back/src/main/java/api/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://2024-qwerty-front-2.vercel.app/") // Permitir origen del frontend
-                //.allowedOrigins("http://localhost:5173") // Permitir origen del frontend
+                .allowedOrigins("http://localhost:5173") // Permitir origen del frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Incluir OPTIONS
                 .allowedHeaders("*")
                 .allowCredentials(true); // Permitir credenciales

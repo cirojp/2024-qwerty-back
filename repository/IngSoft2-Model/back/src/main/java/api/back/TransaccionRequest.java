924242600aaa1cb087740450027910ef42268fb0
Marcos Marcello <marcosmarcello17@gmail.com>
2024-10-12 20:11:42 -0300

    Constructor TransaccionRequest

--- repository/IngSoft2-Model/back/src/main/java/api/back/TransaccionRequest.java
+++ repository/IngSoft2-Model/back/src/main/java/api/back/TransaccionRequest.java
@@ -6,12 +6,20 @@ public class TransaccionRequest {
     private Double valor;
     private String email;
     private String motivo;
-    private String id_reserva;  // Nuevo campo id_reserva
-    private LocalDate fecha;    // Fecha por defecto al crear
+    private String id_reserva; // Nuevo campo id_reserva
+    private LocalDate fecha; // Fecha por defecto al crear
 
     // Constructor
     public TransaccionRequest() {
         this.fecha = LocalDate.now(); // Establecer fecha actual por defecto
+    }
+
+    public TransaccionRequest(Double valor, String email, String motivo, LocalDate fecha) {
+        this.valor = valor;
+        this.email = email;
+        this.motivo = motivo;
+        this.fecha = fecha;
+        this.id_reserva = "0";
     }
 
     // Getters y Setters

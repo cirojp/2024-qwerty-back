1d2027b5b005d8aa29987bf348186a4c4f519611
jp ciro <jciro@itba.edu.ar>
2024-10-31 13:15:02 -0300

    fix recepcion de miembros del grupo

--- repository/IngSoft2-Model/back/src/main/java/api/back/GrupoController.java
+++ repository/IngSoft2-Model/back/src/main/java/api/back/GrupoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,8 +20,12 @@ public class GrupoController {
     @PostMapping("/crear")
     public Grupo crearGrupo(@RequestBody Map<String, Object> payload, Authentication authentication) {
         String nombre = (String) payload.get("nombre");
-        List<String> miembrosEmails = (List<String>) payload.get("miembrosEmails");
+        List<String> miembrosEmails = (List<String>) payload.get("usuarios"); // Cambia 'miembrosEmails' a 'usuarios'
         String creadorEmail = authentication.getName(); // Email del usuario autenticado
+
+        if (miembrosEmails == null) {
+            miembrosEmails = new ArrayList<>(); // Asegúrate de que la lista no sea nula
+        }
 
         return grupoService.crearGrupo(nombre, miembrosEmails, creadorEmail);
     }

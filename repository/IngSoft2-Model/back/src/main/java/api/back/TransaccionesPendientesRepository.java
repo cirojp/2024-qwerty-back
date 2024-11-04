47584edd6b3c4d7fea916d62505a80bf917f7033
jp ciro <jciro@itba.edu.ar>
2024-11-04 00:41:27 -0300

    transacciones pendientex fix grupo_id

--- repository/IngSoft2-Model/back/src/main/java/api/back/TransaccionesPendientesRepository.java
+++ repository/IngSoft2-Model/back/src/main/java/api/back/TransaccionesPendientesRepository.java
@@ -12,5 +12,5 @@ public interface TransaccionesPendientesRepository extends JpaRepository<Transac
     // Método para encontrar una transacción pendiente por ID y usuario
     Optional<TransaccionesPendientes> findByIdAndUserId(Long id, Long userId);
 
-    List<TransaccionesPendientes> findByGrupoId(Long grupoId);
+    List<TransaccionesPendientes> findByGrupoId(Long grupo_id);
 }
